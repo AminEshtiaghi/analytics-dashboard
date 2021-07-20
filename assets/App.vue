34cfb7ba640d182c1f8310e1bfb5e5166b617bee
amin <eshtiaghi.amin@gmail.com>
2021-07-20 10:50:47 +0430

    chore: basics of front-end

---
+++ assets/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <dashboard-controls></dashboard-controls>
+        <dashboard-chart></dashboard-chart>
+    </div>
+</template>
+
+<script>
+    import DashboardControls from './components/Dashboard/DashboardControls.vue';
+    import DashboardChart from './components/Dashboard/DashboardChart.vue';
+
+    export default {
+        components: {
+            DashboardControls,
+            DashboardChart,
+        }
+    }
+</script>
---
+++ assets/components/Dashboard/DashboardControls.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="filter">
+        <div class="filter__container">
+            This is going to be Chart
+        </div>
+    </div>
+</template>
